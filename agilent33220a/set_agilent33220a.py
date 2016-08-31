2328f3c2af61024259f4b145cf094fd8f3f727b8
Bruno Garbin <bruno@sanji.uoa.auckland.ac.nz>
2016-08-31 12:57:29 +1200

    update agilent33220a

--- agilent33220a/set_agilent33220a.py
+++ agilent33220a/set_agilent33220a.py
@@ -8,7 +8,7 @@ import time
 PORT = '5'
 
 class agilent33220a():
-        def __init__(self,query=None,command=None):
+        def __init__(self,query=None,command=None,offset=None,amplitude=None,frequency=None,ramp=None):
             self.command = None
             
             rm = v.ResourceManager('@py')
@@ -28,13 +28,23 @@ class agilent33220a():
                 print '\n'
                 self.exit()
             
-            
+            if amplitude:
+                self.inst.write('VOLT '+amplitude)
+            if offset:
+                self.inst.write('VOLT:OFFS '+offset)
+            if frequency:
+                self.inst.write('FREQ '+frequency)
+                
+            if ramp:
+                self.ramp()
             
             self.exit()
         
-
-        def exit(self):
-            sys.exit()
+        def ramp():
+            self.inst.write()
+            self.inst.write()
+            self.inst.write()
+            self.inst.write()
 
         def write(self,query):
             self.inst.write(query)
@@ -42,6 +52,9 @@ class agilent33220a():
         def read(self):
             rep = self.inst.read()
             return rep
+
+        def exit(self):
+            sys.exit()
 
         def idn(self):
             self.inst.write('*IDN?')
@@ -61,7 +74,11 @@ if __name__ == '__main__':
     parser = OptionParser(usage)
     parser.add_option("-c", "--command", type="str", dest="com", default=None, help="Set the command to use." )
     parser.add_option("-q", "--query", type="str", dest="que", default=None, help="Set the query to use." )
+    parser.add_option("-r", "--ramp", type="float", dest="ramp", default=None, help="Turn on ramp mode." )
+    parser.add_option("-o", "--offset", type="str", dest="off", default=None, help="Set the offset value." )
+    parser.add_option("-a", "--amplitude", type="str", dest="amp", default=None, help="Set the amplitude." )
+    parser.add_option("-f", "--frequency", type="str", dest="freq", default=None, help="Set the frequency." )
     (options, args) = parser.parse_args()
     
     ### Start the talker ###
-    agilent33220a(query=options.que,command=options.com)
+    agilent33220a(query=options.que,command=options.com,ramp=options.ramp,offset=options.off,amplitude=options.amp,frequency=options.freq)
