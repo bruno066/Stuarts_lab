8db1cf68db46a1645fc9e750e019227599580e31
Bruno Garbin <bruno@sanji.uoa.auckland.ac.nz>
2016-10-07 11:56:01 +1300

    update PID_perso

--- PID_perso/PID.py
+++ PID_perso/PID.py
@@ -6,9 +6,16 @@ import sys
 import time
 import vxi11 as vxi
 from numpy import mean,fromstring,int8
-from collections import deque
 
-PORT = '5'
+COLOR = 'ivory'
+
+PLOT_ERROR = True
+if PLOT_ERROR:
+    from collections import deque
+    import gobject
+    from matplotlib.pylab import *
+    from matplotlib.widgets import Slider
+
 
 class PID_perso():
         def __init__(self,query=None,command=None,setpoint=0.1):
@@ -18,11 +25,10 @@ class PID_perso():
             self.D = 0.1 * 10**(-5)     # Td
             self.setpoint = setpoint    # setpoint in volts
             
-            self.previous_error = 0
-            self.integral = 0 
+            self.previous_error = 0.
+            self.integral = 0.
             
             self.UPDATE     = True
-            self.FIRST      = True
             self.size_error = 500
             self.chan       = 'CHAN2'
             self.typ        = 'ASC'
@@ -62,17 +68,38 @@ class PID_perso():
                 print '\n'
                 self.exit()
             
+            print 'Communication okay'
+            
             self.t  = time.time()
             self.dt = 0.1
             
             if PLOT_ERROR:
+                ### Building list and figure ###
                 self.deque_error = deque(list(zeros(self.size_error)))
+                self.fig         = figure(31)
+                self.ax          = self.fig.add_axes([0.1, 0.1, 0.8, 0.6], axisbg=COLOR)
+                self.line_error, = plot(self.deque_error)
+                
+                # Slider declarations
+                self.aP        = self.fig.add_axes([0.1, 0.7, 0.8, 0.08])
+                self.aP_slider = Slider(self.aP_sliderax,'$P$',-1,1,self.P,'%1.2f')
+                self.P_slider.on_changed(self.update_param)
+                self.aI         = self.fig.add_axes([0.1, 0.8, 0.8, 0.08])
+                self.aI_slider = Slider(self.aI_sliderax,'$I$',0,10**3,self.I,'%1.2f')
+                self.I_slider.on_changed(self.update_param)
+                self.aD         = self.fig.add_axes([0.1, 0.9, 0.8, 0.08])
+                self.aD_slider = Slider(self.aD_sliderax,'$D$',10**(-5),10**(-3),self.D,'%1.2f')
+                self.D_slider.on_changed(self.update_param)
+                
+                gobject.idle_add(self.PID_controller_GUI)
             
-            self.PID_controller()
+            else:
+                self.PID_controller()
             
-            print 'Communication okay'
-            
-            self.exit()
+        def update_param(self):
+            self.P = round(self.P_slider.val,2)
+            self.I = round(self.I_slider.val,2)
+            self.D = round(self.D_slider.val,2)
         
         def PID_controller(self):
             ### Controller function ###
@@ -84,24 +111,10 @@ class PID_perso():
                 
                 ## Core of the controller
                 error = self.setpoint - measured_value
-                
-                if PLOT_ERROR:
-                    l.popleft()
-                    l.append(error)
-                    if FIRST:
-                        fig        = figure(31)
-                        a         ,= plot(self.deque_error)
-                        self.FIRST = False
-                    else:
-                        a.set_ydata(self.deque_error)
-                    
-                
                 self.integral = self.integral + error*self.dt
                 derivative = (error - self.previous_error)/self.dt
                 output = self.P*(error + self.I*self.integral + self.D*derivative)
                 self.previous_error = error
-                
-                
                 
                 # Send the value to the function generator
                 self.send_computed_value(output)
@@ -110,9 +123,41 @@ class PID_perso():
                 self.t  = time.time()
                 print self.dt
                 
+        
+        def PID_controller_GUI(self):
+            ### Controller function ###
+            while self.UPDATE:
+                # Get the current value
+                measured_value = self.get_value()
                 
+                print 'average voltage:',measured_value
+                
+                ## Core of the controller
+                error = self.setpoint - measured_value
+                self.integral = self.integral + error*self.dt
+                derivative = (error - self.previous_error)/self.dt
+                output = self.P*(error + self.I*self.integral + self.D*derivative)
+                self.previous_error = error
+                
+                ### Updating list and figure ###
+                self.deque_error.popleft()
+                self.deque_error.append(error)
+                self.line_error.set_ydata(self.deque_error)
+                    
+                # Send the value to the function generator
+                self.send_computed_value(output)
+                
+                self.dt = time.time() - self.t
+                self.t  = time.time()
+                print self.dt
+                
+                draw()
+                return True
+            return False
+            
+            
         def send_computed_value(self,offset):
-            #offset = offset - 0.005                      # arbitrary waveform with -5mV DC and 10mV amp
+            offset = offset - 0.005                      # arbitrary waveform with -5mV DC and 10mV amp
             print 'OFFSET APLLIED:',offset
             if abs(offset)>=5.:
                 self.exit()
@@ -129,7 +174,6 @@ class PID_perso():
             self.scope.write(':WAV:DATA?')
             return mean(eval(self.scope.read_raw()))    #mean(fromstring(self.scope.read_raw()[10:],dtype=int8))
             
-                                    # test get mean(trace) and get(trace and mean with python)
                 
         def exit(self):
             sys.exit()         # to set offset to 0 for disconnecting
